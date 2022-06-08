76ad79cad4b560284422de47ad3f0134e6ba498e
Shubham Kumar Yadav <75082349+ShubhamYv@users.noreply.github.com>
2022-06-08 14:12:02 +0530

    Update codechef- The Lead Game.py

--- codechef- The Lead Game.py
+++ codechef- The Lead Game.py
@@ -1,6 +1,6 @@
 t= int(input())
 p1_score, p2_score, lead, leader=0, 0, 0, 0
-while t!=0:
+for i in range(n):
     p1, p2= map(int, input().split())
     p1_score+= p1
     p2_score+= p2
@@ -13,5 +13,4 @@ while t!=0:
     elif diff<0 and abs(diff)> lead:
         lead= abs(diff)
         leader= 2
-    t-=1
 print(leader, lead)
